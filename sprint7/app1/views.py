d3de662ff3962019816f6c9d5b12f2277d4c0d6f
jmartineztorrez <57723225+jmartineztorrez@users.noreply.github.com>
2020-02-12 17:36:01 +0100

    test
    con render

--- sprint7/app1/views.py
+++ sprint7/app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+#from django.template import Template,Context
 
 # Create your views here.
+def home(request):
+    return render(request,'app1/index.html',{})
+
