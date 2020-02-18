34bdceebaf3100d3daa2dd01be34c18108eccc44
AlionaF5 <57355237+AlionaF5@users.noreply.github.com>
2020-02-18 17:20:34 +0100

    con modificar y eliminar

--- sprint7/app1/urls.py
+++ sprint7/app1/urls.py
@@ -7,7 +7,13 @@ urlpatterns = [
     path('crearLista/', views.crearLista, name='crearLista'),
     path('crearTarea/', views.crearTarea, name='crearTarea'),
     path('consultarTablero/', views.consultarTablero, name='consultarTablero'),
-    path('consultarLista/', views.consultarLista, name='consultarLista'),
-    path('consultarTarea/', views.consultarTarea, name='consultarTarea'),
+    path('consultarLista/<int:id>', views.consultarLista, name='consultarLista'),
+    path('consultarTarea/<int:id>', views.consultarTarea, name='consultarTarea'),
+    path('modificarTablero/<int:id>', views.modificarTablero, name='modificarTablero'),
+    path('modificarLista/<int:id>', views.modificarLista, name='modificarLista'),
+    path('modificarTarea/<int:id>', views.modificarTarea, name='modificarTarea'),
+    path('eliminarTablero/<int:id>', views.eliminarTablero, name='eliminarTablero'),
+    path('eliminarLista/<int:id>', views.eliminarLista, name='eliminarLista'),
+    path('eliminarTarea/<int:id>', views.eliminarTarea, name='eliminarTarea'),
 
 ]
--- sprint7/app1/views.py
+++ sprint7/app1/views.py
@@ -45,14 +45,68 @@ def consultarTablero(request):
 	contexto['objectlist'] = listadotablero
 	return render(request, 'app1/consultarTablero.html', contexto)
 
-def consultarLista(request):
-	listadolista = Lista.objects.all()
+def consultarLista(request, id):
+	listadolista = Lista.objects.filter(fk_tablero_id=id)
 	contexto = {}
 	contexto['objectlist'] = listadolista
 	return render(request, 'app1/consultarLista.html', contexto)
 
-def consultarTarea(request):
-	listadotarea = Tarea.objects.all()
+def consultarTarea(request, id):
+	listadotarea = Tarea.objects.filter(fk_lista_id=id)
 	contexto = {}
 	contexto['objectlist'] = listadotarea
 	return render(request, 'app1/consultarTarea.html', contexto)
+
+def modificarTablero(request,id):
+	template = 'app1/crearTablero.html'#el template al q me va a llamar a esta lista
+	info_tablero = get_object_or_404(Tablero,pk=id)#coger informacion, me da una inf de 404 si no encuentra
+	form = TableroForm(request.POST or None, instance= info_tablero)#como no me interesa un formulario vacio, tengo q decirle q me la llene con un cliente 
+	
+	if form.is_valid():
+		form.save()
+		return redirect('consultarTablero')#lo redirecciono, el nombre de la ruta corresponde al nombre del name del url
+	return render(request,template,{'form':form})
+
+def modificarLista(request,id):
+	template = 'app1/crearLista.html'#el template al q me va a llamar a esta lista
+	info_lista = get_object_or_404(Lista,pk=id)#coger informacion, me da una inf de 404 si no encuentra
+	form = ListaForm(request.POST or None, instance= info_lista)#como no me interesa un formulario vacio, tengo q decirle q me la llene con un cliente 
+	
+	if form.is_valid():
+		form.save()
+		return redirect('consultarLista')#lo redirecciono, el nombre de la ruta corresponde al nombre del name del url
+	return render(request,template,{'form':form})
+
+def modificarTarea(request,id):
+	template = 'app1/crearTarea.html'#el template al q me va a llamar a esta lista
+	info_tarea = get_object_or_404(Tarea,pk=id)#coger informacion, me da una inf de 404 si no encuentra
+	form = TareaForm(request.POST or None, instance= info_tarea)#como no me interesa un formulario vacio, tengo q decirle q me la llene con un cliente 
+	
+	if form.is_valid():
+		form.save()
+		return redirect('consultarTarea')#lo redirecciono, el nombre de la ruta corresponde al nombre del name del url
+	return render(request,template,{'form':form})
+
+def eliminarTablero(request,id):
+	template = 'app1/confElimTablero.html'
+	idtablero = get_object_or_404(Tablero,pk=id)
+	if request.method == 'POST':
+		idtablero.delete()
+		return redirect('consultarTablero')
+	return render(request,template,{'idtablero':idtablero})
+
+def eliminarLista(request,id):
+	template = 'app1/confElimLista.html'
+	idlista = get_object_or_404(Lista,pk=id)
+	if request.method == 'POST':
+		idlista.delete()
+		return redirect('consultarLista')
+	return render(request,template,{'idlista':idlista})
+
+def eliminarTarea(request,id):
+	template = 'app1/confElimTarea.html'
+	idtarea = get_object_or_404(Tarea,pk=id)
+	if request.method == 'POST':
+		idtarea.delete()
+		return redirect('consultarTarea')
+	return render(request,template,{'idtarea':idtarea})
