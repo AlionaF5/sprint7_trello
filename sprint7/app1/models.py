fbcaaa8e3d3e808c8ae63b63daaf8ff03768d22a
jmartineztorrez <57723225+jmartineztorrez@users.noreply.github.com>
2020-02-20 17:46:55 +0100

    ultimo test
    test final

--- sprint7/app1/models.py
+++ sprint7/app1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Tablero(models.Model):
-    nombre_tablero = models.CharField(max_length=50)
+    nombre_tablero = models.CharField(max_length=50,verbose_name='')
 
     def __str__(self):
         return self.nombre_tablero    
@@ -18,3 +18,4 @@ class Tarea(models.Model):
     nombre_tarea = models.CharField(max_length=50)
     descripcion = models.CharField(max_length = 255)
     fk_lista = models.ForeignKey(Lista, on_delete = models.CASCADE)
+
--- sprint7/app1/views.py
+++ sprint7/app1/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render,redirect,get_list_or_404
+from django.shortcuts import render,redirect,get_list_or_404,get_object_or_404
 from django.forms import ModelForm
 from app1.models import Tablero,Lista,Tarea
 
 # Create your views here.
 def home(request):
     return render(request,'app1/base.html',{})
+def tablerocreado(request):
+    return render(request,'app1/tablerocreado.html',{})
+
+def listacreada(request):
+    return render(request,'app1/listacreada.html',{})
+
+def tareacreada(request):
+    return render(request,'app1/tareacreada.html',{})
+
+def salir(request):
+    return render(request,'app1/salir.html',{})
+
 class TableroForm(ModelForm):
     class Meta:
         model =Tablero
@@ -25,7 +37,7 @@ def crearTablero(request):
     form=TableroForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('consultarTablero')
+        return redirect('tablerocreado')
     return render(request,plantilla,{'form':form})
 
 def crearLista(request):
@@ -33,13 +45,14 @@ def crearLista(request):
     form=ListaForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('consultarLista')
+        return redirect('listacreada')
     return render(request,plantilla,{'form':form})
 
 def crearTarea(request):
     form=TareaForm(request.POST or None)
     if form.is_valid():
         form.save()
+        return redirect('tareacreada')
     return render(request, 'app1/crearTarea.html',{'form':form})
 
 def consultarTablero(request):
@@ -49,19 +62,73 @@ def consultarTablero(request):
     contexto['object_list']=listadoTablero
     return render(request,template,contexto)
 
-def consultarLista(request):
+def consultarLista(request,id):
     template='app1/consultarLista.html'
-    listadoLista=Lista.objects.all()
+    listadoLista = Lista.objects.filter(fk_tablero_id=id)
     contexto={}
     contexto['object_list']=listadoLista
     return render(request,template,contexto)
 
-def consultarTarea(request):
+def consultarTarea(request,id):
     template='app1/consultarTarea.html'
-    listadoTarea=Tarea.objects.all()
+    listadoTarea = Tarea.objects.filter(fk_lista_id=id)
     contexto={}
     contexto['object_list']=listadoTarea
     return render(request,template,contexto)
 
+def editarTablero(request,id):
+	template = 'app1/crearTablero.html'#el template al q me va a llamar a esta lista
+	info_tablero = get_object_or_404(Tablero,pk=id)#coger informacion, me da una inf de 404 si no encuentra
+	form = TableroForm(request.POST or None, instance= info_tablero)#como no me interesa un formulario vacio, tengo q decirle q me la llene con un cliente 
+	
+	if form.is_valid():
+		form.save()
+		return redirect('consultarTablero')#lo redirecciono, el nombre de la ruta corresponde al nombre del name del url
+	return render(request,template,{'form':form})
+
+def editarLista(request,id):
+	template = 'app1/crearLista.html'#el template al q me va a llamar a esta lista
+	info_lista = get_object_or_404(Lista,pk=id)#coger informacion, me da una inf de 404 si no encuentra
+	form = ListaForm(request.POST or None, instance= info_lista)#como no me interesa un formulario vacio, tengo q decirle q me la llene con un cliente 
+	
+	if form.is_valid():
+		lista=form.save()
+        
+		return redirect('consultarLista',lista.fk_tablero.id)#lo redirecciono, el nombre de la ruta corresponde al nombre del name del url
+	return render(request,template,{'form':form})
+
+def editarTarea(request,id):
+	template = 'app1/crearTarea.html'#el template al q me va a llamar a esta lista
+	info_tarea = get_object_or_404(Tarea,pk=id)#coger informacion, me da una inf de 404 si no encuentra
+	form = TareaForm(request.POST or None, instance= info_tarea)#como no me interesa un formulario vacio, tengo q decirle q me la llene con un cliente 
+	
+	if form.is_valid():
+		tarea=form.save()
+		return redirect('consultarTarea',tarea.fk_lista.id)#lo redirecciono, el nombre de la ruta corresponde al nombre del name del url
+	return render(request,template,{'form':form})
+
+def eliminarTablero(request,id):
+	template = 'app1/confElimTablero.html'
+	idtablero = get_object_or_404(Tablero,pk=id)
+	if request.method == 'POST':
+		idtablero.delete()
+		return redirect('consultarTablero')
+	return render(request,template,{'idtablero':idtablero})
+
+def eliminarLista(request,id):
+	template = 'app1/confElimLista.html'
+	idlista = get_object_or_404(Lista,pk=id)
+	if request.method == 'POST':
+		idlista.delete()
+		return redirect('consultarLista',idlista.fk_tablero.id)
+	return render(request,template,{'idlista':idlista})
+
+def eliminarTarea(request,id):
+	template = 'app1/confElimTarea.html'
+	idtarea = get_object_or_404(Tarea,pk=id)
+	if request.method == 'POST':
+		idtarea.delete()
+		return redirect('consultarTarea',idtarea.fk_lista.id)
+	return render(request,template,{'idtarea':idtarea})
 
 
